2998fcf00960c26d9165814476a2070f71d5dca7
chlgkdms <haeun3882@gmail.com>
2023-10-05 21:21:37 +0900

    ⚡️ :: Application 네비게이션 아이템 추가

--- app/src/main/java/com/example/yuseongallowancepaymentsandroid/navigation/AppNavigationItem.kt
+++ app/src/main/java/com/example/yuseongallowancepaymentsandroid/navigation/AppNavigationItem.kt
@@ -3,4 +3,6 @@ package com.example.yuseongallowancepaymentsandroid.navigation
 sealed class AppNavigationItem(val route: String) {
 
     object APPLY: AppNavigationItem("apply")
+
+    object Application: AppNavigationItem("application")
 }

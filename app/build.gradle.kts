3d1c6b9a8da34745fd42109838cc4e38a748d8a1
rladmsdh <a01051355543@gmail.com>
2023-11-29 08:45:54 +0900

    ::

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,5 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("androidx.navigation:navigation-compose:2.5.3")
     implementation("androidx.compose.material:material-icons-extended:$1.7.2")
+    implementation("androidx.compose.foundation:foundation:1.4.3")
 }

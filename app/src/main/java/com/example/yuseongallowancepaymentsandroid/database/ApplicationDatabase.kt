548c8e9a967c4798c12ff5f32bdb9a913cf621ef
chlgkdms <haeun3882@gmail.com>
2023-12-12 08:39:02 +0900

    ⚡️ :: DB 구현

---
+++ app/src/main/java/com/example/yuseongallowancepaymentsandroid/database/ApplicationDatabase.kt
@@ -0,0 +1,37 @@
+package com.example.yuseongallowancepaymentsandroid.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.yuseongallowancepaymentsandroid.HonorApplicationFormScreen
+import com.example.yuseongallowancepaymentsandroid.dao.ApplicationDao
+import com.example.yuseongallowancepaymentsandroid.entity.Application
+
+
+@Database(
+    entities = [Application::class],
+    version = 1,
+)
+
+abstract class ApplicationDatabase : RoomDatabase() {
+    abstract fun applicationDao(): ApplicationDao
+
+    companion object {
+        private var instance: ApplicationDatabase? = null
+
+        @Synchronized
+        fun getInstance(context: Context): ApplicationDatabase? {
+            if (instance == null) {
+                synchronized(ApplicationDatabase::class){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        ApplicationDatabase::class.java,
+                        "application-database"
+                    ).build()
+                }
+            }
+            return instance
+        }
+    }
+}

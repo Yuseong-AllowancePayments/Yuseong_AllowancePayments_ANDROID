f4e9ce0c01f5426eb5f98366270225e06f1c398c
chlgkdms <haeun3882@gmail.com>
2023-12-15 20:14:49 +0900

    ⚡️ :: model 추가

---
+++ app/src/main/java/com/example/yuseongallowancepaymentsandroid/model/CourtesyApplicationModel.kt
@@ -0,0 +1,23 @@
+package com.example.yuseongallowancepaymentsandroid.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity("courtesyApplication")
+data class CourtesyApplicationModel(
+    @PrimaryKey val affairsNumber: String, // 보훈번호
+    @ColumnInfo val veteranType: String, // 보훈대상자 구분
+    @ColumnInfo val applicantName: String, // 신청인 성명
+    @ColumnInfo val birthDate: String, // 신청인 생년월일
+    @ColumnInfo val gender: String, // 신청인 성별
+    @ColumnInfo val postAddress: String, // 신청인 우편 번호
+    @ColumnInfo val roadAddress: String, // 신청인 도로명 주소
+    @ColumnInfo val administrativeAddress: String, // 행정동
+    @ColumnInfo val phoneNum: String, // 신청인 전화번호(연락처)
+    @ColumnInfo val bankName: String, // 은행명
+    @ColumnInfo val accountOwner: String, // 예금주 성명
+    @ColumnInfo val accountNumber: String, // 계좌번호
+    @ColumnInfo val applicationDate: String, // 신청일
+    @ColumnInfo val note: String?, // 비고
+)
---
+++ app/src/main/java/com/example/yuseongallowancepaymentsandroid/model/PartnerApplicationModel.kt
@@ -0,0 +1,24 @@
+package com.example.yuseongallowancepaymentsandroid.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity("partnerApplication")
+data class PartnerApplicationModel(
+    @PrimaryKey val affairsNumber: String, // 보훈 번호
+    @ColumnInfo val applicantName: String, // 신청인 성명
+    @ColumnInfo val applicantBirthDate: String, // 신청인 주민번호
+    @ColumnInfo val applicantPostAddress: String, // 신청인 우편번호
+    @ColumnInfo val applicantRoadAddress: String, // 신청인 도로명 주소
+    @ColumnInfo val administrativeAddress: String, // 행정동
+    @ColumnInfo val veteranName: String, // 참전유공자 성명
+    @ColumnInfo val veteranBirthDate: String, // 참전유공자 주민번호
+    @ColumnInfo val veteranDeathDate: String, // 참전유공자 사망일자
+    @ColumnInfo val warName: String, // 참전유공자 구분
+    @ColumnInfo val bankName: String, // 은행명
+    @ColumnInfo val accountNumber: String, // 계좌번호
+    @ColumnInfo val accountName: String, // 예금주 성명
+    @ColumnInfo val applicationDate: String, // 신청일
+    @ColumnInfo val note: String, // 비고
+)
